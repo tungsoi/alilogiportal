9c9d4fe803c0a28c2b82b949c73dfa30d15a128d
tungsoi <thanhtung.atptit@gmail.com>
2020-11-09 15:34:26 +0700

    fixbug

--- app/Admin/Controllers/CartController.php
+++ app/Admin/Controllers/CartController.php
@@ -53,7 +53,7 @@ class CartController extends AdminController
         $grid->column('number', 'STT');
         $grid->column('product_image', 'Ảnh sản phẩm')->lightbox(['width' => 100, 'height' => 100]);
         $grid->shop_name('Tên shop');
-        $grid->product_name('Tên sản phẩm')->width('200px');
+        $grid->product_name('Tên sản phẩm');
         $grid->product_link('Link sản phẩm')->display(function () {
             return "<a href=".$this->product_link." target='_blank'>Link sản phẩm</a>";
         });
