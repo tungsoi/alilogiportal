9cdf8a6c8e96aa8976cad2136fc090b2270027d1
tungsoi <thanhtung.atptit@gmail.com>
2020-12-12 10:37:38 +0700

    fixbug

--- app/Admin/Actions/OrderItem/WarehouseVietnam.php
+++ app/Admin/Actions/OrderItem/WarehouseVietnam.php
@@ -10,6 +10,7 @@ use Encore\Admin\Actions\BatchAction;
 use Encore\Admin\Facades\Admin;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WarehouseVietnam extends BatchAction
 {
@@ -20,99 +21,114 @@ class WarehouseVietnam extends BatchAction
     // chuyển trạng thái sản phẩm từ đã đặt hàng --> đã về kho việt nam
     public function handle(Collection $collection, Request $request)
     {
-        if ($request->ajax()) {
+        DB::beginTransaction();
+        try {
+            if ($request->ajax()) {
+                $order_id = 0;
+                $ids = []; // list tất cả id
+                $orders = []; // list tất cả các order id đã được chọn
 
-            $order_id = 0;
-            $ids = []; // list tất cả id
-            $orders = []; // list tất cả các order id đã được chọn
+                $items = [];
 
-            $items = [];
-
-            // validate
-            foreach ($collection as $model) 
-            {
-                if ($model->status != OrderItem::STATUS_PURCHASE_OUT_OF_STOCK)
-                {
-                    $items[] = $model->id;
-                    $item_code = "SPMH-".str_pad($model->id, 5, 0, STR_PAD_LEFT);
-                    if ($model->status == OrderItem::STATUS_PURCHASE_ITEM_NOT_ORDER) { // chưa đặt hàng
-                        // chưa được đặt hàng
-                        return $this->response()->error('Sản phẩm '.$item_code . ' chưa được đặt hàng. Không thể xác nhận Đã về kho Việt Nam. Vui lòng kiểm tra lại !"')->refresh();
-                    } else if ($model->status == OrderItem::STATUS_PURCHASE_WAREHOUSE_VN) { // về kho việt nam
-                        // nếu trạng thái của sản phẩm là đã về kho Việt Nam
-                        return $this->response()->error("Sản phẩm ".$item_code." đã về kho Việt Nam. Không thể xác nhận Đã về kho Việt Nam. Vui lòng kiểm tra lại !")->refresh();
+                // validate
+                foreach ($collection as $model) {
+                    if ($model->status != OrderItem::STATUS_PURCHASE_OUT_OF_STOCK) {
+                        $items[] = $model->id;
+                        $item_code = "SPMH-".str_pad($model->id, 5, 0, STR_PAD_LEFT);
+                        if ($model->status == OrderItem::STATUS_PURCHASE_ITEM_NOT_ORDER) { // chưa đặt hàng
+                            // chưa được đặt hàng
+                            return $this->response()->error('Sản phẩm '.$item_code . ' chưa được đặt hàng. Không thể xác nhận Đã về kho Việt Nam. Vui lòng kiểm tra lại !"')->refresh();
+                        } elseif ($model->status == OrderItem::STATUS_PURCHASE_WAREHOUSE_VN) { // về kho việt nam
+                            // nếu trạng thái của sản phẩm là đã về kho Việt Nam
+                            return $this->response()->error("Sản phẩm ".$item_code." đã về kho Việt Nam. Không thể xác nhận Đã về kho Việt Nam. Vui lòng kiểm tra lại !")->refresh();
+                        }
                     }
                 }
-            }
 
-             // duyệt từng sản phẩm hợp lệ
-            foreach ($items as $item_id)
-            {
-                $item = OrderItem::find($item_id);
-                $item->status = OrderItem::STATUS_PURCHASE_WAREHOUSE_VN;
-                $item->save();
+                // duyệt từng sản phẩm hợp lệ
+                foreach ($items as $item_id) {
+                    $item = OrderItem::find($item_id);
+                    $item->status = OrderItem::STATUS_PURCHASE_WAREHOUSE_VN;
+                    $item->save();
 
-                $order = PurchaseOrder::find($item->order_id);
-                $order->status = PurchaseOrder::STATUS_IN_WAREHOUSE_VN;
-                $order->save();
-            }
-
-            // check só lượng sản phẩm trong đơn đã về hết chưa.
-            // nếu đã về hết thì chuyển trạng thái thành công.
-
-            foreach ($items as $item_id)
-            {
-                $order = PurchaseOrder::find($item->order_id);
-                if ($order->totalWarehouseVietnamItems() == $order->sumQtyRealityItem() && $order->status != PurchaseOrder::STATUS_SUCCESS)
-                {
-                    $order->status = PurchaseOrder::STATUS_SUCCESS;
-                    $order->success_at = date('Y-m-d H:i:s', strtotime(now()));
+                    $order = PurchaseOrder::find($item->order_id);
+                    $order->status = PurchaseOrder::STATUS_IN_WAREHOUSE_VN;
                     $order->save();
+                }
 
-                    if ($order->status == PurchaseOrder::STATUS_SUCCESS) {
-                        $deposited = $order->deposited; // số tiền đã cọc
-                        $total_final_price = round($order->totalBill() * $order->current_rate); // tổng tiền đơn hiện tại VND
+                // check só lượng sản phẩm trong đơn đã về hết chưa.
+                // nếu đã về hết thì chuyển trạng thái thành công.
 
-                        $customer = $order->customer;
-                        $wallet = $customer->wallet;
+                foreach ($items as $item_id) {
+                    $order = PurchaseOrder::find($item->order_id);
+                    if ($order->totalWarehouseVietnamItems() == $order->sumQtyRealityItem() && $order->status != PurchaseOrder::STATUS_SUCCESS) {
+                        $order->status = PurchaseOrder::STATUS_SUCCESS;
+                        $order->success_at = date('Y-m-d H:i:s', strtotime(now()));
+                        $order->save();
 
-                        $flag = false;
-                        if ($deposited <= $total_final_price)
-                        {
-                            # Đã cọc < tổng đơn -> còn lại : tổng đơn - đã cọc
-                            # -> trừ tiền của khách số còn lại
+                        if ($order->status == PurchaseOrder::STATUS_SUCCESS) {
+                            $deposited = $order->deposited; // số tiền đã cọc
+                            $total_final_price = round($order->totalBill() * $order->current_rate); // tổng tiền đơn hiện tại VND
 
-                            $owed = $total_final_price - $deposited;
-                            $customer->wallet = $wallet - $owed; 
-                            $customer->save();
-                            $flag = true;
-                        } else {
+                            $customer = $order->customer;
+                            $wallet = $customer->wallet;
 
-                            # Đã cọc > tổng đơn 
-                            # -> còn lại: đã cọc - tổng đơn
-                            # -> cộng lại trả khách
+                            $flag = false;
+                            if ($deposited <= $total_final_price) {
+                                # Đã cọc < tổng đơn -> còn lại : tổng đơn - đã cọc
+                                # -> trừ tiền của khách số còn lại
 
-                            $owed = $deposited - $total_final_price;
-                            $customer->wallet = $wallet + $owed; 
-                            $customer->save();
-                            $flag = true;
+                                $owed = $total_final_price - $deposited;
+                                $customer->wallet = $wallet - $owed;
+                                $customer->save();
+                                $flag = true;
+
+                                if ($flag) {
+                                    TransportRecharge::firstOrCreate([
+                                        'customer_id'       =>  $order->customer_id,
+                                        'user_id_created'   =>  1,
+                                        'money'             =>  $owed,
+                                        'type_recharge'     =>  TransportRecharge::PAYMENT_ORDER,
+                                        'content'           =>  'Thanh toán đơn hàng mua hộ. Mã đơn hàng '.$order->order_number,
+                                        'order_type'        =>  TransportRecharge::TYPE_ORDER
+                                    ]);
+                                }
+                                DB::commit();
+                                return $this->response()->success('Xác nhận các sản phẩm đã về kho Việt Nam thành công !')->refresh();
+                            } else {
+
+                                # Đã cọc > tổng đơn
+                                # -> còn lại: đã cọc - tổng đơn
+                                # -> cộng lại trả khách
+
+                                $owed = $deposited - $total_final_price;
+                                $customer->wallet = $wallet + $owed;
+                                $customer->save();
+                                $flag = true;
+
+                                if ($flag) {
+                                    TransportRecharge::firstOrCreate([
+                                        'customer_id'       =>  $order->customer_id,
+                                        'user_id_created'   =>  1,
+                                        'money'             =>  $owed,
+                                        'type_recharge'     =>  TransportRecharge::REFUND,
+                                        'content'           =>  'Thanh toán đơn hàng mua hộ. Mã đơn hàng '.$order->order_number,
+                                        'order_type'        =>  TransportRecharge::TYPE_ORDER
+                                    ]);
+                                }
+        
+                                DB::commit();
+                                return $this->response()->success('Xác nhận các sản phẩm đã về kho Việt Nam thành công !')->refresh();
+                            }
                         }
-
-                        if ($flag) {
-                            TransportRecharge::firstOrCreate([
-                                'customer_id'       =>  $order->customer_id,
-                                'user_id_created'   =>  1,
-                                'money'             =>  $owed,
-                                'type_recharge'     =>  TransportRecharge::PAYMENT_ORDER,
-                                'content'           =>  'Thanh toán đơn hàng mua hộ. Mã đơn hàng '.$order->order_number,
-                                'order_type'        =>  TransportRecharge::TYPE_ORDER
-                            ]);
-                        }
-
-                        return $this->response()->success('Xác nhận các sản phẩm đã về kho Việt Nam thành công !')->refresh();
                     }
                 }
-            }   
+            }
+        }
+        catch (\Exception $e) {
+            DB::rollBack();
+            dd($e->getMessage());
+            return $this->response()->error($e->getMessage())->refresh();
         }
     }
 
